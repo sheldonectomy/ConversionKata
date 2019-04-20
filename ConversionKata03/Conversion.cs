825a7751db474a1ca23e41d83700d0a448b82a0b
Alan Campbell <alan.vine@outlook.com>
2019-04-20 19:41:17 +0100

    Kata 03 - Stop 19:31

---
+++ ConversionKata03/Conversion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionKata03
+{
+    public class Conversion
+    {
+        public string ArabicToRoman(int arabic)
+        {
+            var result = new StringBuilder(string.Empty);
+            var remaining = arabic;
+
+            if(remaining >= 5)
+            {
+                result.Append("V");
+                remaining -= 5;
+            }
+
+            if (remaining == 4)
+            {
+                result.Append("IV");
+                remaining -= 4;
+            }
+
+            for (int i = 0; i < remaining; i++)
+            {
+                result.Append("I");
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ ConversionKata03/ConversionTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionKata03
+{
+    [TestFixture]
+    public class ConversionTests
+    {
+        Conversion _conversion;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _conversion = new Conversion();
+        }
+
+        [Test]
+        public void ConversionToRomanReturnsCorrectValueFrom_0001()
+        {
+            Assert.AreEqual("I", _conversion.ArabicToRoman(1));
+        }
+
+        [Test]
+        public void ConversionToRomanReturnsCorrectValueFrom_0002()
+        {
+            Assert.AreEqual("II", _conversion.ArabicToRoman(2));
+        }
+
+        [Test]
+        public void ConversionToRomanReturnsCorrectValueFrom_0004()
+        {
+            Assert.AreEqual("IV", _conversion.ArabicToRoman(4));
+        }
+
+        [Test]
+        public void ConversionToRomanReturnsCorrectValueFrom_0005()
+        {
+            Assert.AreEqual("V", _conversion.ArabicToRoman(5));
+        }
+
+        [Test]
+        public void ConversionToRomanReturnsCorrectValueFrom_0006()
+        {
+            Assert.AreEqual("VI", _conversion.ArabicToRoman(6));
+        }
+
+    }
+}
